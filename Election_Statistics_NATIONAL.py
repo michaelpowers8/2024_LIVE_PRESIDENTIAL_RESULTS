9ed590405f12f3975a92cd91fca942a33d611a6a
michaelpowers8 <126989418+michaelpowers8@users.noreply.github.com>
2024-11-05 13:36:12 -0600

    Fresh Start before Extraction

--- Election_Statistics_NATIONAL.py
+++ Election_Statistics_NATIONAL.py
@@ -10,7 +10,7 @@ from time import sleep
 
 # WebDriver Chrome
 options = ChromeOptions()
-options.add_argument('--headless=new')
+#options.add_argument('--headless=new')
 # adding argument to disable the AutomationControlled flag 
 options.add_argument("--disable-blink-features=AutomationControlled") 
 # exclude the collection of enable-automation switches 
@@ -44,9 +44,10 @@ while True:
             (By.XPATH, "/html/body"))).text
     lines:list[str] = page.split('\n')
     extraction_time:datetime = datetime.now()
+    print(extraction_time.strftime("%m/%d/%Y, %H:%M:%S.%f"))
     all_states_found:bool = False
     for i,line in enumerate(lines):
-        if('All State Races' in line and 'Alaska' in lines[i+2] and '3' in lines[i+3]):
+        if('All State Results' in line and 'Alaska' in lines[i+2] and '3' in lines[i+3]):
             all_states_found:bool = True
         if(all_states_found):
             if(line in states):
@@ -58,11 +59,11 @@ while True:
                     ]
                 if(first_iteration):
                     DataFrame([current_row],columns=['KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct'],index=[extraction_time])\
-                        .to_csv(f'State_Summary_Results/{line.replace("-","_").replace(" ","_")}_Results.csv',
+                        .to_csv(f'C:/Users/michael/Documents/Election_Statistics/2024_LIVE_PRESIDENTIAL_RESULTS/State_Summary_Results/{line.replace("-","_").replace(" ","_")}_Results.csv',
                                 mode='w',header=True,index=True,float_format='%.3f')
                 else:
                     DataFrame([current_row],columns=['KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct'],index=[extraction_time])\
-                        .to_csv(f'State_Summary_Results/{line.replace("-","_").replace(" ","_")}_Results.csv',
+                        .to_csv(f'C:/Users/michael/Documents/Election_Statistics/2024_LIVE_PRESIDENTIAL_RESULTS/State_Summary_Results/{line.replace("-","_").replace(" ","_")}_Results.csv',
                                 mode='a',header=False,index=True,float_format='%.3f')
         if('Sponsored Stories' in line and all_states_found):
             break
--- Election_Statistics_NORTH_CAROLINA.py
+++ Election_Statistics_NORTH_CAROLINA.py
@@ -5,12 +5,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from pandas import DataFrame
 from random import random
-from re import findall
+from re import search,sub
 from time import sleep
 
 # WebDriver Chrome
 options = ChromeOptions()
-options.add_argument('--headless=new')
+#options.add_argument('--headless=new')
 # adding argument to disable the AutomationControlled flag 
 options.add_argument("--disable-blink-features=AutomationControlled") 
 # exclude the collection of enable-automation switches 
@@ -24,10 +24,15 @@ options.add_argument("--disable-plugins-discovery")
 options.add_argument("--start-maximized")
 
 driver:Chrome = Chrome(options=options)
-sleep(4.53298)
-driver.get("https://www.foxnews.com/elections/2024/general-results/state/north-carolina")
+#sleep(4.53298)
+driver.get("https://www.nbcnews.com/politics/2024-elections/north-carolina-president-results")
+wait:WebDriverWait = WebDriverWait(driver,(random()*4)+2)
+page:str = wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, "/html/body"))).text
+sleep(45)
 first_iteration:bool = True
-sleep(3.32382901)
+counties_found:bool = False
 while True:
     sleep(random()*13.329104+11.430829)
     if(random()<0.0025):
@@ -40,25 +45,27 @@ while True:
     lines:list[str] = page.split('\n')
     extraction_time:datetime = datetime.now()
     print(f"{extraction_time.strftime('%D, %H:%M:%S.%f')}")
-    current_row:list = [0]*7
+    counties_found:bool = False
     for i,line in enumerate(lines):
-        if('Electoral Votes' in line):
-            current_row[4] = float(lines[i+5].replace('%','').replace(',','').replace(' ',''))
-            current_row[5] = int(lines[i+7].replace('%','').replace(',','').replace(' ',''))
-            current_row[6] = int(lines[i+3].replace('%','').replace(',','').replace(' ',''))
-        if('Statewide Results' in line):
-            current_row[0] = int(findall(r"[0-9]+",lines[i+3].replace(',','').replace('-','0'))[0])
-            current_row[1] = float(findall(r"[0-9]+",lines[i+3].replace(',','').replace('-','0'))[1])
-            current_row[2] = int(findall(r"[0-9]+",lines[i+6].replace(',','').replace('-','0'))[0])
-            current_row[3] = float(findall(r"[0-9]+",lines[i+6].replace(',','').replace('-','0'))[1])
+        if('ALAMANCE' in line and not(counties_found)):
+            counties_found:bool = True
+        if('Exit Polls' in line):
+            break
+        if(counties_found and line.upper().__eq__(line) and not(search(r"[0-9]+",line)) and not(line.isspace()) and len(line)>3):
+            current_row = [
+                    int(search(r"[0-9]+",sub(r"[A-Za-z]+","",lines[i+1]).replace(',','').replace('.','').replace('%','')).group()),
+                    int(search(r"[0-9]+",lines[i+5]).group()),
+                    float(search(r"[0-9]+\.[0-9]+",lines[i+6]).group()),
+                    int(search(r"[0-9]+",lines[i+9]).group()),
+                    float(search(r"[0-9]+\.[0-9]+",lines[i+10]).group()),
+                    float(search(r"[0-9]+[\.]{0,1}[0-9]{0,2}\%",lines[i+2]).group().replace(' ','').replace('%',''))
+                ]
             if(first_iteration):
-                DataFrame([current_row],columns=['KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct','Pct_Reported','Est_Votes_Remain','Population'],index=[extraction_time])\
-                    .to_csv(f'State_Details/{"North_Carolina".replace("-","_").replace(" ","_")}_Results.csv',
+                DataFrame([current_row],columns=['Total_Votes','KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct','Pct_Reported'],index=[extraction_time])\
+                    .to_csv(f'State_Details/Wisconsin/{line.replace("-","_").replace(" ","_")}_Results.csv',
                             mode='w',header=True,index=True,float_format='%.3f')
                 first_iteration:bool = False
             else:
-                DataFrame([current_row],columns=['KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct','Pct_Reported','Est_Votes_Remain','Population'],index=[extraction_time])\
-                    .to_csv(f'State_Details/{"North_Carolina".replace("-","_").replace(" ","_")}_Results.csv',
+                DataFrame([current_row],columns=['Total_Votes','KH_Vote_Count','KH_Vote_Pct','DT_Vote_Count','DT_Vote_Pct','Pct_Reported'],index=[extraction_time])\
+                    .to_csv(f'State_Details/Wisconsin/{line.replace("-","_").replace(" ","_")}_Results.csv',
                             mode='a',header=False,index=True,float_format='%.3f')
-            current_row:list = [0]*7
-            break
